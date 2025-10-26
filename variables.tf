e9a1752ea151c5fee5cbd4b310a31f128a0ca70e
Gang Hao <Gang.Hao@centaurihs.com>
2025-10-26 14:06:43 -0500

    3.1

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "test-project-1-473902"
 }
 
 variable "region" {
   description = "Region for resources"
   type        = string
-  default     = "us-south1"
+}
+
+variable "environment" {
+  description = "Target environment"
+  type        = string
 }
