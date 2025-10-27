f3b3b64355eca96a5f3e4aae004627292d3c3ec7
Gang Hao <gang.hao@austin.utexas.edu>
2025-10-27 18:41:46 -0500

    prod 0.0.1

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,3 +1,21 @@
+locals {
+  apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sqladmin.googleapis.com",
+    "servicenetworking.googleapis.com"
+  ]
+}
+resource "google_project_service" "enabled_apis" {
+  for_each = toset(local.apis)
+
+  project = var.project_id
+  service = each.key
+
+  disable_on_destroy = false
+}
+
 resource "google_storage_bucket" "main_bucket" {
   name          = "${var.project_id}-${var.environment}-bucket"
   location      = var.region
