ff8ed1b838778ae5e64017735a03febf3eef1c2c
Gang Hao <gang.hao@austin.utexas.edu>
2025-10-27 23:25:32 -0500

    dev 0.0.2

--- modules/database/postgresql/outputs.tf
+++ modules/database/postgresql/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_name" {
   description = "Name of the Cloud SQL instance"
-  value       = module.postgresql.instance_name
+  value       = module.postgresql_single_zone.instance_name
 }
 
 output "connection_name" {
   description = "Connection string for the instance"
-  value       = module.postgresql.connection_name
+  value       = module.postgresql_single_zone.connection_name
 }
 
 output "public_ip_address" {
   description = "Public IP address of the instance"
-  value       = module.postgresql.public_ip_address
+  value       = module.postgresql_single_zone.public_ip_address
 }
