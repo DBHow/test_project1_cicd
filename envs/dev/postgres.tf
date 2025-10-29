a9a494f93e63b7f262e855c291934ff183e7f4b4
Gang Hao <hao_gang@yahoo.com>
2025-10-29 01:50:16 -0500

    dev 0.3.4

--- envs/dev/postgres.tf
+++ envs/dev/postgres.tf
@@ -1,26 +1,27 @@
-# data "google_secret_manager_secret_version" "db_password" {
-#   project = var.project_id
-#   secret  = "postgres-db-password"
-#   version = "latest"
-# }
+data "google_secret_manager_secret_version" "db_password" {
+  project = var.project_id
+  secret  = "postgres-db-password"
+  version = "latest"
+}
 
-# # Reserve IP range for Private Service Connection
-# resource "google_compute_global_address" "private_ip_address" {
-#   name          = "dev-postgres-private-ip"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   address       = "10.100.0.0"
-#   prefix_length = 16
-#   network       = module.network.network_self_link
-#   project       = var.project_id
-# }
+# Reserve IP range for Private Service Connection
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "dev-postgres-private-ip"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  address       = "10.100.0.0"
+  prefix_length = 16
+  network       = module.network.network_self_link
+  project       = var.project_id
+}
 
-# # Create Private Service Connection
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   network                 = module.network.network_self_link
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
+# Create Private Service Connection
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = module.network.network_self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  deletion_policy         = "ABANDON"
+}
 
 # module "postgresql_db" {
 #   source = "../../modules/database/postgresql"
