ca9abf53cd36fc75e1a458900bada710ecc8e131
Gang Hao <hao_gang@yahoo.com>
2025-10-28 21:31:44 -0500

    dev 0.1.5

--- envs/dev/vm.tf
+++ envs/dev/vm.tf
@@ -1,29 +1,29 @@
 # Create service account for web VM
-resource "google_service_account" "web_vm_sa" {
-  account_id   = "web-vm-sa"
-  display_name = "Web VM Service Account"
-  description  = "Service account for web server VM"
-  project      = var.project_id
+# resource "google_service_account" "web_vm_sa" {
+#   account_id   = "web-vm-sa"
+#   display_name = "Web VM Service Account"
+#   description  = "Service account for web server VM"
+#   project      = var.project_id
 
-  depends_on = [google_project_service.enabled_apis]
-}
+#   depends_on = [google_project_service.enabled_apis]
+# }
 
-# Grant necessary roles to the service account
-resource "google_project_iam_member" "web_vm_sa_log_writer" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.web_vm_sa.email}"
+# # Grant necessary roles to the service account
+# resource "google_project_iam_member" "web_vm_sa_log_writer" {
+#   project = var.project_id
+#   role    = "roles/logging.logWriter"
+#   member  = "serviceAccount:${google_service_account.web_vm_sa.email}"
 
-  depends_on = [google_service_account.web_vm_sa]
-}
+#   depends_on = [google_service_account.web_vm_sa]
+# }
 
-resource "google_project_iam_member" "web_vm_sa_metric_writer" {
-  project = var.project_id
-  role    = "roles/monitoring.metricWriter"
-  member  = "serviceAccount:${google_service_account.web_vm_sa.email}"
+# resource "google_project_iam_member" "web_vm_sa_metric_writer" {
+#   project = var.project_id
+#   role    = "roles/monitoring.metricWriter"
+#   member  = "serviceAccount:${google_service_account.web_vm_sa.email}"
 
-  depends_on = [google_service_account.web_vm_sa]
-}
+#   depends_on = [google_service_account.web_vm_sa]
+# }
 
 module "web_vm" {
   source = "../../modules/vm"
@@ -51,10 +51,12 @@ module "web_vm" {
   }]
 
   # Use custom service account
-  service_account = {
-    email  = google_service_account.web_vm_sa.email
-    scopes = ["cloud-platform"]
-  }
+  # service_account = {
+  #   email  = google_service_account.web_vm_sa.email
+  #   scopes = ["cloud-platform"]
+  # }
+
+  service_account = null
 
   depends_on = [google_project_service.enabled_apis]
 }
