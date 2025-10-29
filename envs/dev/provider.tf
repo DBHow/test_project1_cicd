51817ddb861c7be251a775d73cff1f07e0843858
Gang Hao <hao_gang@yahoo.com>
2025-10-28 22:00:17 -0500

    dev 0.1.9

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = ">= 6.0, < 8.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 6.0, < 8.0.0"
     }
     random = {
       source  = "hashicorp/random"
